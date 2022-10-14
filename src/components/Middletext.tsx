c34b1976bb9d3838265b9aadf93e7048146a0485
yohannes <yohannes222ethiopia@gmail.com>
2022-10-14 12:12:59 +0300

    more ui changes

--- src/components/Middletext.tsx
+++ src/components/Middletext.tsx
@@ -2,7 +2,7 @@ import { Button } from "flowbite-react";
 import { TypeAnimation } from "react-type-animation";
 
 export const MiddleText = () => {
-    
+
     return (
         <div id="middletext" className="py-12 px-6 text-gray-800 text-left whitespace-break-normal">
 
@@ -21,14 +21,19 @@ export const MiddleText = () => {
                 cursor={true}
                 repeat={Infinity}
                 style={{ fontSize: '2.8em' }}
+                className="py-4"
             />
-        
+
             <p>
-                Hi Yohannes here, computer science student. I have been interested in technology since my early ages. I have been writing programs that do simple tasks since I was 17 years old. I am now a mobile and web developer who is passionate about technology. I love collaborating and making connections.My specialties include Java,JavaFx,Android, React, Flutter and kotlin.
+                Hi Yohannes here, a computer science student who constantly seeks out innovative solutions to everyday problems. 
+                I have been interested in technology since my early ages. I have been writing programs that do simple tasks since I was 17. 
+                In those years I've honed my analytical thinking and collaboration skills, and I love working with a team. I love collaborating and making connections.
+                My specialties include Java, Android, React, Flutter and kotlin.
+
                 <br /> <br />
                 I am always open to chatting about tech startups and would love for you to connect with me.Please feel free also to message me if you want a work to be done.
                 <br />
-                Skills: Java, Android, JavaFX, React, Flutter and kotlin.
+                Skills: Java, Android, React, Flutter and kotlin.
                 <br />
                 Open to: Remote jobs and internships
             </p>
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -9,7 +9,7 @@ export const Testimonials = () => {
 
             <div className="grid mx-auto lg:grid-cols-3 sm:grid-cols-1 gap-4">
 
-                <div className="flex flex-col text-left px-5 hover:drop-shadow-xl">
+                <div className="flex flex-col text-left col-span-1 px-5 hover:drop-shadow-xl">
                     <Card>
                         <div className="mb-2">
                             <p className="text-gray-700 text-base">
@@ -28,31 +28,29 @@ export const Testimonials = () => {
                     </Card>
                 </div>
 
-                <div className="flex flex-col text-left px-5 hover:drop-shadow-xl">
+                <div className="flex flex-col text-left px-5 col-span-1 hover:drop-shadow-xl">
                     <Card>
                         <div className="mb-2">
                             <p className="text-gray-700 text-base">
-                                I have been lucky enough and have the opportunity to work with Yohannes. He is passionate, skillful, and most of all fun to work with.
-                                Anyone who values being on top of projects and is looking for a highly competent full-stack/android developer will be glad to work with him.
+                                I have been lucky enough and have the opportunity to work with Yohannes.
                             </p>
                         </div>
 
                         <div className="flex items-center">
-                            <img className="w-10 h-10 rounded-full mr-4" src="/images/ak_ysh.jpeg" alt="Avatar of Jonathan Reinink" />
+                            <img className="w-10 h-10 rounded-full mr-4" src="/images/bes_m.png" alt="Avatar of Jonathan Reinink" />
                             <div className="text-sm">
                                 <p className="text-gray-900 font-medium leading-none">Besufikad M.</p>
-                                <p className="text-gray-600 pt-1">Aug 18</p>
+                                <p className="text-gray-600 pt-1">Software Engineering student at ASTU</p>
                             </div>
                         </div>
                     </Card>
                 </div>
 
-                <div className="flex flex-col text-left px-5 hover:drop-shadow-xl">
+                <div className="flex flex-col text-left px-5 col-span-1 hover:drop-shadow-xl">
                     <Card>
                         <div className="mb-2">
                             <p className="text-gray-700 text-base">
-                                I have been lucky enough and have the opportunity to work with Yohannes. He is passionate, skillful, and most of all fun to work with.
-                                Anyone who values being on top of projects and is looking for a highly competent full-stack/android developer will be glad to work with him.
+                                I have been lucky enough and have the opportunity to work with Yohannes.competent full-stack/android developer will be glad to work with him.
                             </p>
                         </div>
 
