ac2411aa3ab5fb55538eaec673958d043c305278
Yohannes Tezera <52599607+YohannesTz@users.noreply.github.com>
2023-05-01 20:35:36 +0300

    Update Middletext.tsx link to pdf

--- src/components/Middletext.tsx
+++ src/components/Middletext.tsx
@@ -38,7 +38,7 @@ export const MiddleText = () => {
             </p>
             <div className="flex flex-row my-6">
                 <div>
-                    <Button pill href="https://drive.google.com/file/d/1UJZNPPsgJl0aKR7mb8OoUvCzyOzQfx1i/view?usp=share_link">
+                    <Button pill href="https://drive.google.com/file/d/1pONHJzOJzxHbBZtEqU05fqLju8hR4iOc/view?usp=share_link">
                         Download Resume
                     </Button>
                 </div>
